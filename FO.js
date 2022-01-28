cb13598f729d3a24e9a5d5f1b49059840ff5f99b
Rohit Tyagi <92664353+MrRohitTyagi@users.noreply.github.com>
2022-01-28 18:44:37 +0530

    Add files via upload

--- FO.js
+++ FO.js
@@ -1,14 +1,13 @@
-const { Console } = require('console')
 const fs = require('fs')
 const path = require('path')
-const { send } = require('process')
+
 let input = process.argv.slice(2) // used to take input from  integrated terminal as array and slice will take all indexes after 0 and 1
 let inputArr = input  // inputArr will now have an array with elements after 2nd index  ie [node FO.js ke baad jitne bhe elementss honge vo sub inputArr  array mai aa jynge]
 
 let command = inputArr[0] //command mai inputArr array ka first index store ho jyega
 // console.log(command)
 let types = {
-    media: ["mp4", "mkv", "mp3","PNG","jpeg","jpg"],
+    media: ["mp4", "mkv", "mp3", "PNG", "jpeg", "jpg"],
     archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
     documents: ["docx", "doc", "pdf", "xlsx", "xls", "odt", "ods", "odp", "odg", "odf", "txt", "ps", "tex",],
     app: ["exe", "dmg", "pkg", "deb"],
@@ -16,7 +15,7 @@ let types = {
 
 
 switch (command) { //switch  takes a input as command and matches it with all the cases if the  case matches with the command the coresponding text with that case will gets execuited
-    case 'tree': console.log('tree implemented')
+    case 'tree': treefn(inputArr[1])
         break //  case agar command se match ho jata ha toh uske neeche vale bhe sub chal jate hai ,so we use break 
     case 'organise': organisefn(inputArr[1])
         break
@@ -77,9 +76,9 @@ function organisehelper(src, dest) {
 
         if (isfile == true) {
             let filecategory = getcategory(childnames[i])//calling another function to get the extension of all the files
-            console.log(childnames[i],'belongs to' ,filecategory)
-            sendfiles(childaddress,dest,filecategory)
-            
+            console.log(childnames[i], 'belongs to', filecategory)
+            sendfiles(childaddress, dest, filecategory)
+
 
         }
     }
@@ -91,32 +90,65 @@ function organisehelper(src, dest) {
 function getcategory(filename) {
     let ext = path.extname(filename).slice(1)//slice(1) will remove "." from the extension so that is matches the  keys of object declared above
     // console.log(ext)
-    
-    for(let key in types){ //loop for each key in the object above
+
+    for (let key in types) { //loop for each key in the object above
         let cTYpeArr = types[key]
         // console.log(cTYpeArr)
-        for(let i=0;i<cTYpeArr.length;i++){ 
-            if(ext==cTYpeArr[i]){ // matching each extension with the object file and if it matches key is returned else others is returned
+        for (let i = 0; i < cTYpeArr.length; i++) {
+            if (ext == cTYpeArr[i]) { // matching each extension with the object file and if it matches key is returned else others is returned
                 return key
             }
-            
-            }
-        }
-        return 'others'
-    }
-    
-function sendfiles(srcfilePath , destfilepath,filecatagory){  //function for creating folders  corresponding to keys in objects
 
-    let catPAth = path.join(destfilepath,filecatagory)
-    if(fs.existsSync(catPAth)==false){ // check  if same name folder already present or not 
+        }
+    }
+    return 'others'
+}
+
+function sendfiles(srcfilePath, destfilepath, filecatagory) {  //function for creating folders  corresponding to keys in objects
+
+    let catPAth = path.join(destfilepath, filecatagory)
+    if (fs.existsSync(catPAth) == false) { // check  if same name folder already present or not 
         fs.mkdirSync(catPAth) //if not then mane a folder 
     }
-    let fileName= path.basename(srcfilePath)
-    let destFilePAth = path.join(catPAth,fileName) 
-    fs.copyFileSync(srcfilePath,destFilePAth)
+    let fileName = path.basename(srcfilePath)
+    let destFilePAth = path.join(catPAth, fileName)
+    fs.copyFileSync(srcfilePath, destFilePAth)
     console.log('files organised')
 
 
-}    
-    
+}
 
+
+function treefn(dirpath) {
+    if (dirpath == undefined) {
+        console.log('please enter a valid derectory path')
+        return;
+    }
+    else {
+        let direxists = fs.existsSync(dirpath)
+        if (direxists == true) {
+            treehelper(dirpath,' ')
+        }
+    }
+
+}
+function treehelper(targetPath,indent){
+    let isFile = fs.lstatSync(targetPath).isFile()
+    if(isFile == true){
+        let fileName = path.basename(targetPath)
+        console.log(indent+"├─"+fileName)
+    }
+    else{
+        let dirName = path .basename(targetPath)
+        console.log(indent+"└──"+dirName)
+
+        let children = fs.readdirSync(targetPath)
+        // console.log(children)
+        for(let i = 0;i<children.length;i++){
+            let childpath = path.join(targetPath,children[i])
+            treehelper(childpath,indent+'\t')
+        }
+
+    }
+
+}
